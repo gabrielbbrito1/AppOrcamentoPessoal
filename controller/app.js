d3256d14f66692e6d1ff8999ebe8972b7347f179
Gabriel <gabrielbbrito_1@hotmail.com>
2023-02-28 14:48:58 -0300

    adicionando controle dos ids e parte da validação dos dados

--- controller/app.js
+++ controller/app.js
@@ -1,3 +1,5 @@
+
+// classe do cadastro da despesa
 class Despesa{
     constructor(ano, mes, dia, tipo, descricao, valor){
         this.ano = ano
@@ -7,7 +9,44 @@ class Despesa{
         this.descricao = descricao
         this.valor = valor
     }
+    validarDados(){
+        for(let i in this){
+            if(this[i] == undefined || this[i] == '' || this[i] == null){
+                return false
+            }
+        }
+    }
 }
+
+class Bd{
+    constructor(){
+        // inicializa o primeiro id em 0 se for nulo
+        let id = localStorage.getItem('id')
+
+        if (id === null){
+            localStorage.setItem('id', 0)
+        }
+    }
+
+    getProximoId(){
+        // controla os ids das despesas, acrescentando 1 ao id a cada nova gravação
+        let proximoId = localStorage.getItem('id')
+        return parseInt(proximoId) + 1
+    }
+
+    gravar(d){
+        // seta o item no id informado
+        localStorage.setItem(id , JSON.stringify(d))
+
+        // executa a função getProximoId, onde acrescenta o valor do id + 1
+        let id = this.getProximoId()
+
+        localStorage.setItem('id', id)
+    }
+}
+
+let bd = new Bd()
+
 let btnAdd = document.getElementById('btnAdd')
 
 
@@ -20,11 +59,13 @@ btnAdd.addEventListener("click", function(){
     let valor = document.getElementById('valor')
 
     let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
-    
-    gravar(despesa)
+    despesa.validarDados()
+
+    despesa.validarDados() ? bd.gravar(despesa) : console.log('Erro')
 })
 
-function gravar(d){
-    localStorage.setItem('despesa', JSON.stringify(d))
-}
+
+
+
+
 
